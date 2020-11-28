696bd5bc61db633574a5475a1c81456ca840898c
zied sayadi <zieed.sayadi@gmail.com>
2020-11-28 17:22:12 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This the BetterMotherFuckingWebsiteWithPopup
+
+you can check this url for more information : https://ziedsayadi1992.github.io/BetterMotherFuckingWebsiteWithPopup/
---
+++ main.js
@@ -0,0 +1,19 @@
+
+const alertPop = document.querySelector('.alert_container') ;
+const overlay = document.getElementById('overlay');
+const siteBtn = document.querySelector('[data-site-target]')
+const closeBtn = document.querySelector('[data-close]')
+
+document.addEventListener('DOMContentLoaded',()=>{
+    alertPop.classList.add('active')
+    overlay.classList.add('active')
+    siteBtn.addEventListener('click',()=>{
+        window.open('https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+        alertPop.classList.remove('active')
+        overlay.classList.remove('active')
+    })
+    closeBtn.addEventListener('click',()=>{
+        alertPop.classList.remove('active')
+        overlay.classList.remove('active')
+    })
+})
